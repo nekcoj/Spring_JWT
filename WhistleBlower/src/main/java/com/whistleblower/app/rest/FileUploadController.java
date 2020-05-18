97fb5cdb38267945ccc3aa63efa0059533d56b6d
codeItRalf <codeItRalf@users.noreply.github.com>
2020-05-18 14:46:21 +0200

    upload/ download added and working

---
+++ WhistleBlower/src/main/java/com/whistleblower/app/rest/FileUploadController.java
@@ -0,0 +1,58 @@
+package com.whistleblower.app.rest;
+
+import com.whistleblower.app.storage.StorageFileNotFoundException;
+import com.whistleblower.app.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public  FileUploadController(StorageService storageService){
+        this.storageService = storageService;
+    }
+
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename){
+
+        Resource file = storageService
+.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename()
+        + "\"").body(file);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file")MultipartFile file){
+
+        storageService.store(file);
+
+        String filename = file.getOriginalFilename();
+        if(filename != null){
+            return ResponseEntity.ok( filename + " was successfully uploaded");
+        }else {
+            return ResponseEntity.badRequest().body("File not uploaded");
+        }
+
+
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?>
+    handleStorageFileNotFound(StorageFileNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+}
