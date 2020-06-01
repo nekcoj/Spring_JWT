bd288223408909b37d7ff5e6a0fed8f0620c410a
nekcoj <55090544+nekcoj@users.noreply.github.com>
2020-06-01 14:37:43 +0200

    fixed uncaught exception: Object (was trying to reroute to currentpage)

--- front/src/store/account.module.js
+++ front/src/store/account.module.js
@@ -1,61 +1,62 @@
-import { userService } from '../_services';
-import router from '../router';
-import store from '../store'
+import { userService } from "../_services";
+import router from "../router";
+import store from "../store";
 
-const user = JSON.parse(localStorage.getItem('user'));
+const user = JSON.parse(localStorage.getItem("user"));
 const state = user
-    ? { status: { loggedIn: true }, user }
-    : { status: {}, user: null };
+  ? { status: { loggedIn: true }, user }
+  : { status: {}, user: null };
 
 const actions = {
-    login({ commit }, { username, password }) {
-        commit('loginRequest', { username });
-        userService.login(username, password)
-            .then(
-                user => {
-                    commit('loginSuccess', user);
-                    store.state.authUser = user.path;
-                    store.state.gdprConsent = user.consent;
-                    if(!user.consent && !(user.path === '/user')){
-                      router.push('/inloggning')
-                    } else {
-                      router.push(user.path)
-                    }
-                },
-                error => {
-                    commit('loginFailure', error);
-                    console.log("Failed to login!")
-                }
-            );
-    },
-    logout({ commit }) {
-        userService.logout();
-        commit('logout');
-    }
+  login({ commit }, { username, password }) {
+    commit("loginRequest", { username });
+    userService.login(username, password).then(
+      (user) => {
+        commit("loginSuccess", user);
+        store.state.authUser = user.path;
+        store.state.gdprConsent = user.consent;
+        if (!user.consent && !(user.path === "/user")) {            
+            if(router.currentRoute.fullPath != '/inloggning') {
+                router.push({path: "/inloggning"});
+            }
+        } else {
+          router.push(user.path);
+        }
+      },
+      (error) => {
+        commit("loginFailure", error);
+        console.log("Failed to login!");
+      }
+    );
+  },
+  logout({ commit }) {
+    userService.logout();
+    commit("logout");
+  },
 };
 
 const mutations = {
-    loginRequest(state, user) {
-        state.status = { loggingIn: true };
-        state.user = user;
-    },
-    loginSuccess(state, user) {
-        state.status = { loggedIn: true };
-        state.user = user;
-    },
-    loginFailure(state) {
-        state.status = {};
-        state.user = null;
-    },
-    logout(state) {
-        state.status = { loggedIn: false};
-        state.user = null;
-    }
+  loginRequest(state, user) {
+    state.status = { loggingIn: true };
+    state.user = user;
+  },
+  loginSuccess(state, user) {
+    state.status = { loggedIn: true };
+    state.user = user;
+  },
+  loginFailure(state) {
+    state.status = {};
+    state.user = null;
+  },
+  logout(state) {
+    state.status = { loggedIn: false };
+    state.user = null;
+  },
 };
 
 export const account = {
-    namespaced: true,
-    state,
-    actions,
-    mutations
+  namespaced: true,
+  state,
+  actions,
+  mutations,
 };
